16f81dbf47a1cc941265c605d5a8e2218221ac7e
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-12-27 12:35:55 +0530

    setup client side login and register page

--- Doctor Appointment App/client/src/pages/Login.js
+++ Doctor Appointment App/client/src/pages/Login.js
@@ -1,10 +1,28 @@
 
+import React from 'react';
+import '../styles/RegisterStyle.css'
+import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Login = ()=>{
+    const onfinishHandler = (values)=>{
+        console.log(values)
+     }
     return(
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <div className='form-container'>
+            <Form layout='vertical' onFinish={onfinishHandler} className='register-form'>
+                <h3 className='text-center'>Login Form</h3>
+               
+                <Form.Item label="Email" name="email">
+                    <Input type='email' required/>
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input type='password' required/>
+                </Form.Item>
+                <p>Don`t have an account <Link to="/register">here</Link></p>
+                <Button type='submit' className='btn btn-primary'>Login</Button>
+            </Form>
+         </div>
     )
 }
 
--- Doctor Appointment App/client/src/pages/Register.js
+++ Doctor Appointment App/client/src/pages/Register.js
@@ -1,10 +1,31 @@
-
+import React from 'react';
+import '../styles/RegisterStyle.css'
+import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Register = ()=>{
+    const onfinishHandler = (values)=>{
+       console.log(values)
+    }
     return(
-        <div>
-            <h1>Register Page</h1>
-        </div>
+       <>
+         <div className='form-container'>
+            <Form layout='vertical' onFinish={onfinishHandler} className='register-form'>
+                <h3 className='text-center'>Register Form</h3>
+                <Form.Item label="Name" name="name">
+                    <Input type='text' required/>
+                </Form.Item>
+                <Form.Item label="Email" name="email">
+                    <Input type='email' required/>
+                </Form.Item>
+                <Form.Item label="Password" name="password">
+                    <Input type='password' required/>
+                </Form.Item>
+                <p>Already have an account <Link to="/login">here</Link></p>
+                <Button type='submit' className='btn btn-primary'>Register</Button>
+            </Form>
+         </div>
+       </>
     )
 }
 
