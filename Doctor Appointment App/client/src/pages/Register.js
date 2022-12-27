af1c1110594e7a153f3d5fc0ad36c9b09d804596
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-12-27 18:47:13 +0530

    completed register functionality

--- Doctor Appointment App/client/src/pages/Register.js
+++ Doctor Appointment App/client/src/pages/Register.js
@@ -1,28 +1,45 @@
 import React from 'react';
-import '../styles/RegisterStyle.css'
-import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import '../styles/RegisterStyle.css';
+import axios from 'axios';
+import { Form, Input,message } from 'antd';
+import { Link,useNavigate } from 'react-router-dom';
 
 const Register = ()=>{
-    const onfinishHandler = (values)=>{
-       console.log(values)
+    let navigate = useNavigate();
+
+    const onFinishHandler = async(values)=>{
+        console.log(values)
+        try{
+           const res = await axios.post(`/api/v1/users/register`,values);
+           if(res.data.success){
+            message.success('Register Successfully!')
+            navigate('/login')
+           }
+           else{
+            message.error(res.data.message)
+           }
+        }
+        catch(error){
+            console.log(error)
+            message.error('something went wrong')
+        } 
     }
     return(
        <>
-         <div className='form-container'>
-            <Form layout='vertical' onFinish={onfinishHandler} className='register-form'>
+         <div className="form-container">
+            <Form layout="vertical" onFinish={onFinishHandler} className="register-form">
                 <h3 className='text-center'>Register Form</h3>
                 <Form.Item label="Name" name="name">
-                    <Input type='text' required/>
+                    <Input type="text" required/>
                 </Form.Item>
                 <Form.Item label="Email" name="email">
-                    <Input type='email' required/>
+                    <Input type="email" required/>
                 </Form.Item>
                 <Form.Item label="Password" name="password">
-                    <Input type='password' required/>
+                    <Input type="password" required/>
                 </Form.Item>
                 <p>Already have an account <Link to="/login">here</Link></p>
-                <Button type='submit' className='btn btn-primary'>Register</Button>
+                <button type="submit" className='btn btn-primary'>Register</button>
             </Form>
          </div>
        </>
