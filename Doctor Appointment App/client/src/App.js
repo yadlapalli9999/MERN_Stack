33c9c00da54e878fe8b9fac70867666de510d9ed
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-12-27 11:30:44 +0530

    setup client side

---
+++ Doctor Appointment App/client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+      <h1 className="text-success">Hello</h1>
+    </div>
+    </>
+    
+  );
+}
+
+export default App;
