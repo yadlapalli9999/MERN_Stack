034a60627c42e0ee04ebd812e5593c37e1201b01
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-12-28 15:28:41 +0530

    layout setup for client side

--- Doctor Appointment App/client/src/pages/HomePage.js
+++ Doctor Appointment App/client/src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React,{useEffect} from 'react';
 import axios from 'axios';
+import Layout from '../components/Layout';
 
 const HomePage = ()=>{
     const getUserData = async()=>{
@@ -19,9 +20,9 @@ const HomePage = ()=>{
        getUserData();
     },[])
     return(
-        <div>
+        <Layout>
             <h1>Home Page</h1>
-        </div>
+        </Layout>
     )
 }
 
